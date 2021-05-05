db4f277aa44ce1228c3f061bfe602f342ded5d80
drewrob <werd.roberts27@gmail.com>
2021-05-05 15:15:48 -0500

    Part 2 for Drew

--- Payroll.java
+++ Payroll.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
+
 public class Payroll
 {
    public static void main(String[] args)
    {
-      int hours = 40;
-      double grossPay, payRate = 25.0;
+	  Scanner scnr = new Scanner(System.in);
+	  System.out.println("Enter your name: ");
+	  String name = scnr.nextLine();
+	  System.out.println("Enter hours worked: ");
+      int hours = scnr.nextInt();
+	  System.out.println("Enter pay rate: ");
+      double grossPay, payRate = scnr.nextDouble();
 
       grossPay = hours * payRate;
-      System.out.println("Your gross pay is $" + grossPay);
+      System.out.println(name + "'s gross pay is $" + grossPay);
+      System.out.println(name + " worked " + hours + " hours with a $" + payRate + " payrate;");
+      
+      
+      
    }
 }
