2b193c35b1558ed8c2b3ef311cead3b1163f1ea1
garcia <ogarcia@gmail.com>
2021-05-05 15:05:39 -0500

    omuiris changes

--- payroll.java
+++ payroll.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 public class Payroll
 {
    public static void main(String[] args)
    {
+	   Scanner scnr = new Scanner(System.in);
+	   
       int hours = 40;
       double grossPay, payRate = 25.0;
 
       grossPay = hours * payRate;
       System.out.println("Your gross pay is $" + grossPay);
+      
+     
+      
+      
+     System.out.println("Enter your hours worked");
+      
    }
 }
