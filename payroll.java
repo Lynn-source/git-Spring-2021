c6175c39bfdd5b8b473ad36ed484aff2c3396d87
ja404724 <jay.alshouse@midlandu.edu>
2021-05-05 15:06:43 -0500

    Jay Alshouse

--- payroll.java
+++ payroll.java
@@ -1,11 +1,19 @@
-public class Payroll
+import java.util.Scanner;
+
+
+
+public class payroll
 {
    public static void main(String[] args)
    {
+	   Scanner scnr = new Scanner (System.in);
       int hours = 40;
-      double grossPay, payRate = 25.0;
+      System.out.println("Enter your pay");
+      System.out.println("Enter your hours");
+      double grossPay, payRate = scnr;
 
       grossPay = hours * payRate;
-      System.out.println("Your gross pay is $" + grossPay);
+      System.out.println( grossPay);      
+      
    }
 }
