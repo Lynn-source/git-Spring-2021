7708c30921700af796a6df55c4d382bcf5e952d0
MichaelWailes <83610024+MichaelWailes@users.noreply.github.com>
2021-05-05 15:19:10 -0500

    Add files via upload

--- payroll.java
+++ payroll.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
 public class Payroll
+
+
 {
    public static void main(String[] args)
    {
-      int hours = 40;
-      double grossPay, payRate = 25.0;
+	  Scanner scnr = new Scanner(System.in);
+	  System.out.println("Please enter hours worked.");
+      int hours = scnr.nextInt();
+      System.out.println("Please enter your payrate");
+      double grossPay, payRate = scnr.nextDouble();
 
-      grossPay = hours * payRate;
+      double overtime = 0.0;
+      double OvertimePay = 0.0;
+      if(hours > 40)
+      {
+    	 overtime = hours - 40;
+    	 OvertimePay = (overtime * payRate) * 1.5;
+      }
+      grossPay = hours * payRate + OvertimePay;
+      System.out.println("Your overtime pay is $"+ OvertimePay);
       System.out.println("Your gross pay is $" + grossPay);
    }
 }
